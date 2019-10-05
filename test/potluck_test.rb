a470b84ae609ce6362d739c035f30b32fd87045f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-05 16:40:02 -0600

    Add get_all_from_category test

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -8,6 +8,9 @@ class PotluckTest < Minitest::Test
     @potluck = Potluck.new("7-13-18")
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
   end
 
   def test_is_has_a_class
@@ -30,5 +33,17 @@ class PotluckTest < Minitest::Test
     assert_equal [@couscous_salad, @cocktail_meatballs], @potluck.dishes
 
     assert_equal 2, @potluck.dishes.length
+  end
+
+  def test_get_all_from_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@candy_salad)
+
+    assert_equal [@couscous_salad, @summer_pizza], @potluck.get_all_from_category(:appetizer)
+    assert_equal @couscous_salad, @potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", @potluck.get_all_from_category(:appetizer).first.name
   end
 end
