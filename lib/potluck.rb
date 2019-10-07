1d9f13a7e88de30318eeeab4456e9a7dbe123756
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-07 08:28:28 -0600

    Add menu method and ratio init

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -14,4 +14,18 @@ class Potluck
       dish if dish.category == category
     end
   end
+
+  def menu
+    menu = @dishes.sort_by do |dish|
+      dish.name
+    end
+
+    menu.group_by do |dish|
+      dish.category
+    end
+  end
+
+  def ratio(category)
+
+  end
 end
